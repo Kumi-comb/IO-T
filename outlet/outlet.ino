e5141367132ff515efcf40ebd3b630aad5ca1997
zen510 <44563532+zen510@users.noreply.github.com>
2019-06-21 18:03:36 +0900

    スイッチON/OFFまで

--- outlet/outlet.ino
+++ outlet/outlet.ino
@@ -6,11 +6,35 @@
 
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
+
 #include <Wire.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 
 Adafruit_SSD1306 display(128,64,&Wire,2); //displayインスタンス作成 エラーが出た場合は2を5に変えてみる
 
-void setup(){
+String httpGET(String url) {
+    HTTPClient client;
+
+    client.begin(url);
+    int statusCode = client.GET();
+
+    String result = "";
+    if (statusCode < 0) {
+        result = client.errorToString(statusCode);
+    } else {
+        result = client.getString();
+    }
+    client.end();
+
+    return result;
+}
+
+void setup() {
+
+    Serial.begin(9600);
+
+    pinMode(14, OUTPUT);
 
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  //ディスプレイ初期化
     display.clearDisplay();
@@ -35,5 +59,22 @@ void setup(){
 }
 
 void loop(){
-    
+    String json = httpGET("http://iot.nsrv.work/api/devices/2/queue");
+
+    const size_t jsonSize = JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(6);
+    DynamicJsonBuffer jsonBuffer(jsonSize);
+    JsonObject& root = jsonBuffer.parseObject(json);
+
+    const char* value = root["value"];
+    String valueStatus = String(value);
+
+    Serial.println(json);
+
+    if (valueStatus == "ON") {
+        digitalWrite(14, HIGH);
+    } else if (valueStatus == "OFF") {
+        digitalWrite(14, LOW);
+    }
+
+    delay(5000);
 }
