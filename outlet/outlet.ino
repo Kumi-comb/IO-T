5b4e24692b856f65ad79895bf6a9a0a54f3ae92e
Tomotada Saigusa <nirot1r@g-second.net>
2019-06-21 23:31:16 +0900

    リファクタリング

--- outlet/outlet.ino
+++ outlet/outlet.ino
@@ -1,17 +1,19 @@
 #include <FS.h>
-#include <ESP8266WiFi.h>
+
 #include <DNSServer.h>
-#include <ESP8266WebServer.h>
 #include <WiFiManager.h> 
 
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+
+#include <Wire.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
 
-#include <Wire.h>
-#include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 
-Adafruit_SSD1306 display(128,64,&Wire,2); //displayインスタンス作成 エラーが出た場合は2を5に変えてみる
+Adafruit_SSD1306 display(128, 64, &Wire, 2); //displayインスタンス作成 エラーが出た場合は2を5に変えてみる
 
 String beforeStatus = "";
 
@@ -52,26 +54,29 @@ String httpPUT(String url, String contentType, String data) {
 
 void setDeviceStatus(String status) {
     StaticJsonBuffer<4> jsonBuffer;
+
     JsonObject& root = jsonBuffer.createObject();
     root["type"] = "TOGGLE";
     root["value"] = status;
 
     String data;
     root.printTo(data);
+
     httpPUT("http://iot.nsrv.work/api/devices/2/status", "application/json", data);
 }
 
 void setup() {
-
     Serial.begin(9600);
 
     pinMode(14, OUTPUT);
 
-    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  //ディスプレイ初期化
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //ディスプレイ初期化
+
     display.clearDisplay();
+
     display.setTextSize(2);
     display.setTextColor(WHITE);
-    display.println(String("AP Mode"));  //WiFiManagerのSSIDはESP  + MACアドレスの末尾3バイト
+    display.println(String("AP Mode")); //WiFiManagerのSSIDはESP  + MACアドレスの末尾3バイト
     display.print("ID:");
     display.println(ESP.getChipId());
     display.display(); //バッファの内容を表示 これが無いと適用されない バッファの内容を表示 これが無いと適用されない 
@@ -80,11 +85,14 @@ void setup() {
     wifimanager.autoConnect(); //以前接続したときの設定で接続できなかった場合はAPモードになり設定画面をだす おそらく接続できるまで次にいかない
     
     display.clearDisplay();
+    
     display.setCursor(0,0);
     display.println(String("Connected."));
     display.println(WiFi.SSID());
     display.display();//バッファの内容を表示 これが無いと適用されない バッファの内容を表示 これが無いと適用されない 
+    
     delay(5000);
+
     display.clearDisplay();
     display.display();//バッファの内容を表示 これが無いと適用されない バッファの内容を表示 これが無いと適用されない 
 }
@@ -108,7 +116,7 @@ void loop() {
             digitalWrite(14, LOW);
         }
 
-        beforeStatus = nowStatus;s
+        beforeStatus = nowStatus;
     }
 
     delay(5000);
