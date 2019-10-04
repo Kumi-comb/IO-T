abc5fa45e6630812288952ff719480ec549ae1cf
zen510 <44563532+zen510@users.noreply.github.com>
2019-10-04 14:38:28 +0900

    センサ読み取りまでできるはず

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -11,7 +11,7 @@
 Adafruit_SSD1306 display(128,64,&Wire,2); //displayインスタンス作成 エラーが出た場合は2を5に変えてみる
 
 void setup(){
-
+    Wire.begin();
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  //ディスプレイ初期化
     display.clearDisplay();
     display.setTextSize(2);
@@ -34,6 +34,33 @@ void setup(){
     display.display();//バッファの内容を表示 これが無いと適用されない バッファの内容を表示 これが無いと適用されない 
 }
 
-void loop(){
+    const uint8_t ADR = 0x5c;
+    uint8_t data[8];
 
+void loop(){
+    Wire.beginTransmission(ADR);
+    Wire.endTransmission();
+    delayMicroseconds(500);
+
+    Wire.beginTransmission(ADR);
+    Wire.write(0x03);
+    Wire.write(0x00);
+    Wire.write(0x04);
+    Wire.endTransmission();
+
+    delayMicroseconds(1500);
+    Wire.requestFrom(ADR,8);
+    delayMicroseconds(3);
+    if (Wire.available() >= 8) {
+        for(uint8_t i = 0; i = 8 ; i ++){
+            data[i] = Wire.read();
+        }
+    }
+
+    float hm = ((float)(data[2]*256+data[3])) / 10;
+    float tp = ((float)(data[4]*256+data[5])) / 10;
+    
+    display.setCursor(0,0);
+    display.println(hm);
+    display.println(tp);
 }
